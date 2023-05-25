96c260445d2b20ca801e7e56703e30656b2c2d47
erick1005 <erickmonari1@gmail.com.com>
2023-05-25 19:15:25 +0300

    update

--- 3-shell.c
+++ 3-shell.c
@@ -79,10 +79,8 @@ int populate_env_list(info_t *info)
 	list_t *node = NULL;
 	size_t i;
 
-	while (i != '\0')
-	{
-		add_node_end(&node, 0, 0);
-		i++;
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
 	}
 	info->env = node;
 	return (0);
